87b923e4dbb91bcf5cb69cfa93c58f6d529187fe
sachin <sachinp@shriffle.com>
2023-08-11 19:27:24 +0530

    Validation Practice

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+	validates :name , presence: {strict: true}, length:{minimum:3} , allow_blank:false
+	validates :age , comparison: { greater_than: 18 }
+	validates :num, numericality: {only_integer: true}
+	validates :address, inclusion:{in: %w(small), message: "not a valid input"}, allow_nil:true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+class CheckValidation < ActiveModel::Validator
+	def validate(record)
+		if record.name == "sachin"
+			record.errors.add :base ,"My Name Is Sachin"
+		end
+	end
+end
 class User < ApplicationRecord
+	validates_with  CheckValidation
 end
